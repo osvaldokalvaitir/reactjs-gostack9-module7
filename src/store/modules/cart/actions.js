250887a639f1fbf78dc189661c22dc7714cbf654
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-11-10 18:03:34 -0300

    Reactotron + Saga

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,13 +1,13 @@
 export function addToCartRequest(id) {
   return {
-    type: '@cart/REQUEST',
+    type: '@cart/ADD_REQUEST',
     id,
   };
 }
 
 export function addToCartSuccess(product) {
   return {
-    type: '@cart/SUCCESS',
+    type: '@cart/ADD_SUCCESS',
     product,
   };
 }
